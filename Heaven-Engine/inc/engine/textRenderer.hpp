38896ac16dd6675419ce3df8cf139ab2d788e9ae
Rihay66 <noiralter0660@gmail.com>
2023-10-17 19:12:20 -0400

    Forgot to mention that the text renderer depending on platform gets the freetype library, moved glad.c to it's own folder in source, and continued to plan and work on the text renderer

--- Heaven-Engine/inc/engine/textRenderer.hpp
+++ Heaven-Engine/inc/engine/textRenderer.hpp
@@ -7,10 +7,17 @@
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
 #include <resourceSys/shader.hpp>
 #include <resourceSys/texture.hpp>
+
+// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2   Size;      // size of glyph
+    glm::ivec2   Bearing;   // offset from baseline to left/top of glyph
+    unsigned int Advance;   // horizontal offset to advance to next glyph
+};
 
 class TextRenderer{
     private:
@@ -18,10 +25,19 @@ class TextRenderer{
         unsigned int VAO, VBO;
 
     public:
+        //Hold a map of pre-compiled characters
+        std::map<char, Character> Characters;
+
+        //Shader used for text rendering
+        Shader textShader;
+
         //Constructor       
         TextRenderer(unsigned int width, unsigned int height);
         //Destructor
         ~TextRenderer();
+
+        void loadFont(std::string filename, unsigned int fontSize);
+        
 
 
 };
--- Heaven-Engine/src/engine/textRendering.cpp
+++ Heaven-Engine/src/engine/textRendering.cpp
@@ -1,15 +1,19 @@
 #include <engine/textRenderer.hpp>
 
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 
+//Check platform and then grab the Freetype library
 #ifdef __unix__ //Linux platform
     #include <freetype2/ft2build.h>
     #include FT_FREETYPE_H
-#elif defined(_WIN32) || defined(WIN32) //WIndows platform
+#elif defined(_WIN32) || defined(WIN32) //Windows platform
     #include <ft2build.h>
     #include FT_FREETYPE_H
 #endif
 
+//TODO: When creating the texture use the glad given id num to bind the texture
+//TODO: Make batch rendering when rendering text
 
 TextRenderer::TextRenderer(unsigned int width, unsigned int height){
     
