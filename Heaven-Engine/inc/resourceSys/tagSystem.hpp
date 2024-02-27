0c59bc15cf11c486480e4293d1b297b312e06d15
Rihay66 <noiralter0660@gmail.com>
2024-02-27 18:34:51 -0500

    Fixing the format of the comments of the tag system

--- Heaven-Engine/inc/resourceSys/tagSystem.hpp
+++ Heaven-Engine/inc/resourceSys/tagSystem.hpp
@@ -3,11 +3,11 @@
 #ifndef TAGSYSTEM_HPP
 #define TAGSYSTEM_HPP
 
-//include standard libraries
+// include standard libraries
 #include <string>
 #include <vector>
 
-//include gameobject class
+// include gameobject class
 #include <gameObjs/gameObject.hpp>
 
 /* A Static singleton TagSystem class that hosts several
@@ -19,42 +19,43 @@ class TagSystem{
     public:
         //*Adder Funcs
 
-        //add a tag and object to the resource pool, if object already exists then replace it's tag
+        // add a tag and object to the resource pool, if object already exists then replace it's tag
         static void addTag(std::string tagName, GameObject* gameObj);
 
         //*Getter Funcs
 
-        //get a tag from a object in the tag pool
+        // get a tag from a object in the tag pool
         static std::string getTag(GameObject* gameObj);
         /* get object from the tag pool by tag
             ! Only use this function only when there's a singular object with it's own special tag
             ! it will not return multiple objects the same tag
         */
         static GameObject* getObject(std::string tagName);
-        //check if tag pertains to a passed object that exists in tag pool
+        // check if tag pertains to a passed object that exists in tag pool
         static bool checkObject(std::string target, GameObject* gameObj);
-        //get amount of objects in the tag system
+        // get amount of objects in the tag system
         static int getAmountOfObjects();
 
         //*Remover Funcs
 
-        //get object and remove from list
+        // get object and remove from list
         static void removeObject(GameObject* gameObj);
 
-        //get and remove objects with a certain tag
+        // get and remove objects with a certain tag
         static void removeObjectsWithTag(std::string& tagName);
 
+        // remove references and clear tag pool
         static void clear();
 
     private:
-        //define data type that contains both tag and gameobject
+        // define data type that contains both tag and gameobject
         struct objectTag{
             std::string name;
             GameObject* obj;
         };
-        // Static resource. Contains GameObjects alongside with their assigned tag
+        // static resource. Contains GameObjects alongside with their assigned tag
         static std::vector<objectTag> tagPool;
-        //private constructor
+        // private constructor
         TagSystem(){}
 };
 
