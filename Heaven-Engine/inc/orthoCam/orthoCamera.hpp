bc6746b0e96c2be21ac11f3ca8c42a6e32e5409f
Rihay66 <noiralter0660@gmail.com>
2024-02-27 22:41:14 -0500

    Organizing the resource manager and the orthographic camera

--- Heaven-Engine/inc/orthoCam/orthoCamera.hpp
+++ Heaven-Engine/inc/orthoCam/orthoCamera.hpp
@@ -7,38 +7,37 @@
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 
-//Camera class
+// Camera class
 class Camera{
-    public:
-        //declare const vars
+    protected:
+        // declare const vars
         const glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
         const glm::vec3 right = glm::vec3(1.0f, 0.0f, 0.0f);
 
-        //delcare projection var
-        glm::mat4 projection;
-        glm::mat4 view;
-        glm::mat4 projectionView;
+    public:
+        // maxtrix variable to be used to calculate the 
+        glm::mat4 projection, view, projectionView, transform;
 
-        //reference to the camera position
+        // reference to the camera position
         glm::vec3 position = glm::vec3(0.0f);
 
-        //reference to the shader that can be changed at anytime
+        // reference to the shader that can be changed at anytime
         Shader shader;
 
-        //reference to the GLFW window handle
+        // reference to the GLFW window handle
         GLFWwindow* window;
 
-        //reference to the window size
+        // reference to the window size
         unsigned int width, height;
 
-        //Construcotr
-        Camera(unsigned int Width, unsigned int Height, GLFWwindow* handle,
+        // construcotr
+        Camera(unsigned int width, unsigned int height, GLFWwindow* handle,
         Shader &shader);
 
-        //Destructor
+        // destructor
         ~Camera();
 
-        //Does multiplication of the view and projection to create the ProjectionView
+        // does multiplication of the view and projection to create the ProjectionView
         virtual void calculateProjectionView();
 };
 
--- Heaven-Engine/src/orthoCam/orthoCamera.cpp
+++ Heaven-Engine/src/orthoCam/orthoCamera.cpp
@@ -1,27 +1,30 @@
 #include <orthoCam/orthoCamera.hpp>
 
-Camera::Camera(unsigned int Width, unsigned int Height, GLFWwindow* handle, Shader &shader) : 
-width(Width), height(Height), window(handle), shader(shader){  
-    //update projection and add to shader
+Camera::Camera(unsigned int w, unsigned int h, GLFWwindow* handle, Shader &shdr) : 
+width(w), height(h), window(handle), shader(shdr){  
+    // update projection and add to shader
     calculateProjectionView();
 }
 
 Camera::~Camera(){
-    //Forget the window handle
+    // forget the window handle
     this->window = nullptr;
 }
 
-void Camera::calculateProjectionView()
-{
+void Camera::calculateProjectionView(){
+    // create the projection
     this->projection = glm::ortho(0.0f, static_cast<float>(this->width), 
         0.0f, static_cast<float>(this->height), -1.0f, 1.0f);
 
-    glm::mat4 transform = glm::translate(glm::mat4(1.0f), this->position) * glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0,0,1));
+    // set up the transform 
+    glm::mat4 transform = glm::translate(glm::mat4(1.0f), this->position) 
+    * glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(0,0,1));
 
+    // set up the view using the inverse of the transform
     this->view = glm::inverse(transform);
 
-    //put together the projectionView
+    // put together the projectionView
     this->projectionView = this->projection * this->view;
-    //tell shader to set view
+    // tell shader to set projectionView
     this->shader.SetMatrix4("projectionView", this->projectionView, true);
 }
