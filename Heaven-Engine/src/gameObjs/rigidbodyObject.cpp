606f8ec9dffd3cbce020588bb90be00e9c69df03
Rihay66 <noiralter0660@gmail.com>
2024-02-28 22:54:06 -0500

    Creating a Java translation of the Engine

--- Heaven-Engine/src/gameObjs/rigidbodyObject.cpp
+++
@@ -1,4 +0,0 @@
-#include <gameObjs/rigidbodyObject.hpp>
-
-PhysicsObject::PhysicsObject(int sprt, glm::vec2 pos, glm::vec2 size, glm::vec4 color ,bool interpolation, bool destroyed) : 
-GameObject(sprt, pos, size, color, interpolation), isDestroyed(destroyed){}
