a8b7b578a0d9a36fd403d1c2f85a3c73b6205ff6
Rihay66 <noiralter0660@gmail.com>
2024-12-29 15:35:13 -0500

    Renamed DisplayText() to DrawText(), and made some header comments fixes

--- 3Sisters-Engine/inc/engine/text_renderer.hpp
+++ 3Sisters-Engine/inc/engine/text_renderer.hpp
@@ -11,23 +11,25 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-// include resource manager
-#include <resourceSystems/managers/resource_manager.hpp>
+// include resource types
+#include <resourceSystems/resource_types.hpp>
+#include <resourceSystems/resource_shader.hpp>
 
 /* A static singleton Text Renderer Class used to render text
  with a given font. This Class utilizes fonts loaded from the 
  ResourceManager Class. All functions and resources are static 
  and no public constructor is defined.  
- ! This Class is unoptimized and not very flexible thus 
- take into consideration of its current design
+ @ This Class is unoptimized and not very flexible thus take into consideration of its current design
 */
 class TextRenderer{
     public:        
         // initialize the text renderer which requires the width and height of the display area and a loaded shader
         static void Init(unsigned int width, unsigned int height, Shader& shader);
 
-        // Requires a font set from the ResourceManager, along with attributes of the text being displayed
-        static void DisplayText(std::map<char, Character>& chars, std::string text, glm::vec2 position, glm::vec2 scale, glm::vec4 color = glm::vec4(1.0f));
+        /* used to draw text on the screen using a loaded font
+            @ Recommended to use a font loaded through from the ResourceManager
+        */ 
+        static void DrawText(std::map<char, Character>& chars, std::string text, glm::vec2 position, glm::vec2 scale, glm::vec4 color = glm::vec4(1.0f));
     
     private:
         // stores data of a quad
